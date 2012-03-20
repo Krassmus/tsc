105a7e72c44b6ccd15718b8cf91457caa73b5fb4
Rasmus Fuhse <Krassmus@gmail.com>
2012-03-20 21:54:46 +0100

    config as draft file

---
+++ config.draft.php
@@ -0,0 +1,11 @@
+<?php
+
+$DB_TYPE = 'mysql';       //only mysql works yet
+$DB_HOST = 'localhost';
+$DB_NAME = 'tsc';
+$DB_USER = 'root';
+$DB_PASSWORD = '';
+
+
+$salt1 = "114a644e512737f83d885fc1820f8d11";
+$salt2 = "119ad12d96e6f58b7a5b47c74a896e11";
