6c657dac8312975af9eeb792098f12b4d8363159
Rasmus Fuhse <Krassmus@gmail.com>
2012-08-04 07:32:36 +0200

    add background to antique_theme

--- V/controller/bureau/views/notes.view.php
+++ V/controller/bureau/views/notes.view.php
@@ -1,19 +1,6 @@
 <h1>Notizzettel</h1>
-<style>
-form[name=notizzettel] textarea {
-    background-color: #555555;
-    width: 80%;
-    margin: 10px;
-    margin-left: auto;
-    margin-right: auto;
-    color: white;
-    border: #222222;
-    display: block;
-    padding: 5px;
-}
-</style>
 <form name="notizzettel">
-    <textarea name="notiz" style="width: 80%; height: 200px" onchange="TSC.bureau.set('notiz', this)"><?= escape($notiz) ?></textarea>
+    <textarea name="notiz" style="width: 100%; height: 200px" onchange="TSC.bureau.set('notiz', this)"><?= escape($notiz) ?></textarea>
 </form>
 
 
