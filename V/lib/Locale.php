0491f78342584267c911aa9fabbc73379033183c
Rasmus Fuhse <Krassmus@gmail.com>
2012-08-07 23:35:26 +0200

    add string-replacements to headers and prepare future localization

---
+++ V/lib/Locale.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once dirname(__file__)."/locale/I18nConnector.php";
+
+function l($text, $language_id = null) {
+    $translation = Locale::translate($text, $language_id);
+    $translated = $text !== $translation;
+    return $translated 
+        ? '<span class="translation '.$language_id.'" data-original-string="'.escape($text).'">'.escape($translation).'</span>'
+        : '<span class="nottranslated" data-original-string="'.escape($text).'">'.escape($translation).'</span>';
+}
+
+function ll($text, $language_id = null) {
+    return Locale::translate($text, $language_id);
+}
+
+class Locale {
+    static protected $replacements = array();
+    
+    static public function translate($text, $language_id = null) {
+        $language_id or $language_id = $GLOBALS['user_language'];
+        $translation = I18nConnector::get()->translate($language_id, $text);
+        $translation or $translation = $text;
+        $replacements = (array) self::$replacements[$language_id];
+        return str_replace(array_keys($replacements), $replacements, $translation);
+    }
+    
+    static public function setReplacement($language_id, $word, $replacement) {
+        self::$replacements[$language_id][$word] = $replacement;
+    }
+    
+}
---
+++ V/lib/locale/TranslateDB.interface.php
@@ -0,0 +1,19 @@
+<?php
+
+interface TranslateDB {
+    
+    /**
+     * returns an array of languages as an array itself with 'id' and 'name' as attributes
+     * @return: array(array('id' => "de_DE", 'name' => "Deutsch"), ...) 
+     */
+    public function getLanguages();
+    
+    public function getOrigins();
+    
+    public function getStrings($language_id, $searchword = null, $origin = null);
+    
+    public function translate($language_id, $string);
+    
+    public function add($language_id, $text, $translation, $origin);
+    
+}
